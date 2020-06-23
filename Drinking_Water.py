93de6724668af7df3e62220de62c704c67619410
Rohit <ry700400@gmail.com>
2020-06-23 14:47:50 +0530

    commited

---
+++ Drinking_Water.py
@@ -0,0 +1,11 @@
+import time
+from plyer import notification
+if __name__=='__main__':
+    while(True):
+
+        notification.notify(
+            title="Please Drink Water Now!",
+            message='It is very necessory for health',
+            timeout=3
+        )
+        time.sleep(60*60)
